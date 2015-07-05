2b283b3e7fcf5e6d931ceb04f095edcdeaa23411
Leibniz.Hu <Leibnizhu@gmail.com>
2015-07-05 13:18:16 +0800

    Create Money.java

---
+++ 2015.07.05/Money.java
@@ -0,0 +1,11 @@
+/*EnumDemo---Leibniz.Hu 2015.07.05
+* Execises from <Thinking in Java> (TIJ in short)
+* Page 107 Execise 5.21 &5.22
+* Write a Money enum
+@author Leibniz.Hu
+@version 1.0.0
+*/
+
+public enum Money {
+	ONE__FEN, FIVE_FEN, TEN__FEN, FIFTY_FEN, ONE_YUAN, FIVE_YUAN, TEN_YUAN, TWENTY_YUAN
+}
