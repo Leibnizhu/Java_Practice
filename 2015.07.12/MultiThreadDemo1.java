c82e6e235a02bc8af874eb026246b3212efb4d40
Leibnizhu <Leibnizhu@gmail.com>
2015-07-12 23:05:07 +0800

    Update Multi Thread Demo, Just for try

---
+++ 2015.07.12/MultiThreadDemo1.java
@@ -0,0 +1,31 @@
+/*MultiThreadDemo1---Leibniz.Hu 2015.07.12.
+* Try multi thread.
+* This execise is from video cource.
+@author Leibniz.Hu
+@version 1.0.0
+*/
+
+class TestThread extends Thread{
+	TestThread(String name){
+		super(name);
+	}
+	public void run(){
+		for(int x=0; x<20; x++){
+			System.out.println("Thread: " + Thread.currentThread().getName() + "...x = " + x);
+		}
+		System.out.println("Thread: " + Thread.currentThread().getName() + "...OVER");
+	}
+}
+
+class MultiThreadDemo1{
+	public static void main(String[] args){
+		TestThread testTh1 = new TestThread("Cudy");
+		testTh1.start();
+		TestThread testTh2 = new TestThread("佐藤さん");
+		testTh2.start();
+		for(int x=0; x<20; x++){
+			System.out.println("Thread: " + Thread.currentThread().getName() + "...x = " + x);
+		}
+		System.out.println("Thread: " + Thread.currentThread().getName() + "...OVER");
+	}
+}
