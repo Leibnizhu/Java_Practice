ce50a227238fe25f60322d069cc728c22ba06fe9
Leibniz.Hu <Leibnizhu@gmail.com>
2015-08-31 23:01:20 +0800

    Create Book.java

---
+++ 2015.08.31/Book.java
@@ -0,0 +1,53 @@
+package leibniz.hu;
+
+public class Book {
+	
+	private String id;
+	private String title;
+	private String price;
+	
+	public String getId(){
+		return this.id;
+	}
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public Book() {
+	}
+
+	public Book(String id, String title, String price) {
+		this.id = id;
+		this.title = title;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id  + ", title=" + title  + ", price=" + price + "]";
+	}
+	
+	
+	
+
+	
+}
